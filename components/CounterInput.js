546db101dcd65f06ef81265e8ad43b5f7fde1b6d
march-dave <ysangy@gmail.com>
2017-09-15 20:19:20 -0400

    up to date

--- components/CounterInput.js
+++ components/CounterInput.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import {View, Text} from 'react-native';
 
 class CounterInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 0
+        }
+    }
+
+    onIncremenet(diff) {
+        this.setState(
+           { total : diff }
+        )
+    }
+
     render() {
+
         return (
             <View>
                 <Text>Text Input</Text>
+                <input onPress={this.onIncremenet.bind(this)} />
             </View>
         );
     }
